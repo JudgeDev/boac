660c123b482586d91192863f5e2f7912000a5f70
judgedev <judge.py@gmail.com>
2020-03-15 16:09:19 +0100

    Added templates and static files

--- app/server.py
+++ app/server.py
@@ -1,13 +1,21 @@
+# standard library imports
+from io import BytesIO
+
+# related third party imports
 import aiohttp
 import asyncio
 import uvicorn
 from fastai import *
 from fastai.vision import *
-from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+# local imports
+
+templates = Jinja2Templates(directory='templates')
 
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
@@ -16,9 +24,9 @@ classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
+# allow cross-origin requests from browsers
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-app.mount('/static', StaticFiles(directory=path/'static'))  # needed to add path for local env
-
+app.mount('/static', StaticFiles(directory='static'))  # needed to add path for local env
 
 async def download_file(url, dest):
     if dest.exists(): return
@@ -46,15 +54,19 @@ async def setup_learner():
 
 
 loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future(setup_learner())]
-learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+#tasks = [asyncio.ensure_future(setup_learner())]
+#learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
 
 @app.route('/')
 async def homepage(request):
-    html_file = path / 'view' / 'index.html'
-    return HTMLResponse(html_file.open(encoding="utf-8").read())  # need to add encoding for local env
+    template = 'index.html'
+    context = {"request": request}
+    return templates.TemplateResponse(template, context)
+
+    #html_file = path / 'view' / 'index.html'
+    #return HTMLResponse(html_file.open(encoding="utf-8").read())  # need to add encoding for local env
 
 
 @app.route('/analyze', methods=['POST'])
